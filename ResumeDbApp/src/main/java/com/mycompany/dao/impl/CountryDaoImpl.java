96c0a9bb553c5d1e0997ad2959160ac14f635bef
SahilAppay <sahilappayev@gmail.com>
2020-02-13 22:37:17 +0400

    new commit

--- ResumeDbApp/src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
+++ ResumeDbApp/src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
@@ -9,22 +9,54 @@ import com.mycompany.dao.AbstractDao;
 import com.mycompany.dao.inter.CountryDaoInter;
 import com.mycompany.entity.Country;
 import com.mycompany.entity.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  * @author SahilAppayev
  */
-public class CountryDaoImpl extends AbstractDao implements CountryDaoInter{
+public class CountryDaoImpl extends AbstractDao implements CountryDaoInter {
 
     @Override
     public List<Country> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Country> result = new ArrayList<>();
+        try (Connection connection = connect()) {
+            Statement statement = connection.createStatement();
+            statement.execute("select * from country");
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String countryName = rs.getString("country_name");
+                String nationality = rs.getString("nationality");
+                result.add(new Country(id, countryName, nationality));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
     }
 
     @Override
-    public Country getById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Country getById(int countryId) {
+        Country result = new Country();
+        try (Connection connection = connect()) {
+            Statement statement = connection.createStatement();
+            statement.execute("select * from country where id="+countryId);
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String countryName = rs.getString("country_name");
+                String nationality = rs.getString("nationality");
+                result = new Country(id, countryName, nationality);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
     }
 
     @Override
@@ -42,6 +74,4 @@ public class CountryDaoImpl extends AbstractDao implements CountryDaoInter{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-   
-    
 }
--- ResumeDbApp/src/main/java/com/mycompany/dao/impl/SkillDaoImpl.java
+++ ResumeDbApp/src/main/java/com/mycompany/dao/impl/SkillDaoImpl.java
@@ -9,22 +9,52 @@ import com.mycompany.dao.AbstractDao;
 import com.mycompany.dao.inter.SkillDaoInter;
 import com.mycompany.entity.Skill;
 import com.mycompany.entity.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  * @author SahilAppayev
  */
-public class SkillDaoImpl extends AbstractDao implements SkillDaoInter{
+public class SkillDaoImpl extends AbstractDao implements SkillDaoInter {
 
     @Override
     public List<Skill> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Skill> result = new ArrayList<>();
+        try (Connection connection = connect()) {
+            Statement statement = connection.createStatement();
+            statement.execute("select * from skill");
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result.add(new Skill(id, name));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
     }
 
     @Override
-    public Skill getById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Skill getById(int skillId) {
+        Skill result = new Skill();
+        try (Connection connection = connect()) {
+            Statement statement = connection.createStatement();
+            statement.execute("select * from skill where id="+skillId);
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result = new Skill(id, name);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
     }
 
     @Override
@@ -42,6 +72,4 @@ public class SkillDaoImpl extends AbstractDao implements SkillDaoInter{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-  
-    
 }
